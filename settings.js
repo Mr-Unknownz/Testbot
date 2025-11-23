93c0953c8817d5d126cd3fb36bb92ef16a0c532d
Mr Unknown <168432129+vijitharanawakage@users.noreply.github.com>
2025-11-23 11:35:53 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID || 'QJUSMY=mwZFnaiI#7INSxpGDLQgG_3FPmpvr_hS8GJkpjoUGpAhgGAkWBOE',
+SESSION_ID: process.env.SESSION_ID || 'QJUSMY=7pggXQZI#jRxm68i0Q8XVNmbUONc4vDIhfw-XuVJhv0Cz13qZeRQ',
 MONGODB: process.env.MONGODB || "mongodb://mongo:hSKOyytRSHAikyAFUajcmkoJTEcgoBTR@trolley.proxy.rlwy.net:29757",  
 PREFIX: process.env.PREFIX || '.',
 ALIVE_IMG: process.env.ALIVE_IMG || 'https://files.catbox.moe/xo1wiq.png',   
