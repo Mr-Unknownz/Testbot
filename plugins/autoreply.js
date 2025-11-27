bbaf70ccad8e129b6bff8c247d19be17503fac5c
ranawakagevijitha <121442429+ranawakagevijitha@users.noreply.github.com>
2025-11-27 17:29:16 +0530

    Update autoreply.js

--- plugins/autoreply.js
+++ plugins/autoreply.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const config = require('../settings')
+const config = require('../settings/settings.js')
 const {cmd , commands} = require('../lib/command')
 
 //auto reply 
