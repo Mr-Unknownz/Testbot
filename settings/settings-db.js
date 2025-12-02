b9c886a43d57feecb46bd4a431849a56e821a82b
ranawakagevijitha <121442429+ranawakagevijitha@users.noreply.github.com>
2025-12-02 10:33:52 +0530

    Update settings-db.js

--- settings/settings-db.js
+++ settings/settings-db.js
@@ -3,9 +3,9 @@ const fs = require('fs');
 const path = require('path');
 const axios = require('axios');
 
-const GITHUB_USER = process.env.GITHUB_USER || 'ranawakagevijitha';
-const GITHUB_REPO = process.env.GITHUB_REPO || 'Testbot';
-const GITHUB_TOKEN = process.env.GITHUB_TOKEN || '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';
+const GITHUB_USER = config.GITHUB_USER || 'ranawakagevijitha';
+const GITHUB_REPO = conf8g.GITHUB_REPO || 'Testbot';
+const GITHUB_TOKEN = config.GITHUB_TOKEN || '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';
 const REMOTE_PATH = 'settings/settings.json';
 const LOCAL_PATH = path.join(__dirname, 'local-settings.json');
 
